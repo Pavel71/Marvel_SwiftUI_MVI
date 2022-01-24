c499fc72f57de30c85878c755a0db70a7390d1b2
Pavel Mishagin <xstyle71@gmail.com>
2022-01-24 12:20:58 +0300

    Add root project

---
+++ Marvel_SwiftUI_MVI/Marvel_SwiftUI_MVI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel_SwiftUI_MVI
+//
+//  Created by Павел Мишагин on 24.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
